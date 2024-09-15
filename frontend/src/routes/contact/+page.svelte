08b9c2f7c1e6f2671a83810123669e21b9f740a7
Linus Thorsell <thorsell.linus@gmail.com>
2024-09-15 23:09:26 +0200

    Adding contact us page

---
+++ frontend/src/routes/contact/+page.svelte
@@ -0,0 +1,6 @@
+<div class="flex flex-col justify-center items-center" style="height: calc(100vh - 4.5rem);">
+  <h1 class="mb-5 text-3xl font-bold text-center">Vill du kontakta oss?</h1>
+  <p class="mb-2 text-xl">Kontakta oss via email <a href="mailto:kontakta@teamontheline.com" class="text-primary-600 underline">kontakta@teamontheline.com</a></p>
+  <p class="mb-2 text-xl">Du kan även kontakta oss via <a href="https://www.facebook.com/TeamOnTheLine" class="text-primary-600 underline">Facebook Messenger</a></p>
+  <p style="padding-bottom: 15%;" class="text-xl">Eller via <a href="https://www.instagram.com/ontheline_tour" class="text-primary-600 underline">Instagram</a></p>
+</div>
