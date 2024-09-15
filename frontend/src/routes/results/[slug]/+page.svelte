e63c6912d63e02088c160a5a1e544301c91aba23
Linus Thorsell <thorsell.linus@gmail.com>
2024-09-16 00:09:45 +0200

    Cleaning up

--- frontend/src/routes/results/[slug]/+page.svelte
+++ frontend/src/routes/results/[slug]/+page.svelte
@@ -30,7 +30,7 @@
 <div class="flex flex-col justify-center items-center mt-6">
   <h1 class="sm:text-3xl text-2xl font-bold text-center">Resultat för {data.tourLeaderboard.tour.title}</h1>
   <h2 class="text-lg text-center">De {data.tourLeaderboard.tour.score_count} bästa poängen räknas!</h2>
-  <Accordion flush class="mt-3 lg:w-1/2 w-11/12 mt-4" activeClass="bg-blue-100 dark:bg-gray-800 text-blue-600 dark:text-white focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-800" inactiveClass="text-gray-500 dark:text-gray-400 hover:bg-gray-700 dark:hover:bg-gray-800">
+  <Accordion flush class="mt-3 xl:w-1/2 w-11/12 mt-4" activeClass="bg-blue-100 dark:bg-gray-800 text-blue-600 dark:text-white focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-800" inactiveClass="text-gray-500 dark:text-gray-400 hover:bg-gray-700 dark:hover:bg-gray-800">
     {#each sortDivisions(data.tourLeaderboard.divisions) as division}
       <AccordionItem class="w-full">
         <span slot="header">{division[0]}</span>
