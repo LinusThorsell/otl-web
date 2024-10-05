d8154f69008a6b2628c94c8b2565824250ffcc2a
Linus Thorsell <thorsell.linus@gmail.com>
2024-10-05 23:49:30 +0200

    Updating eventcard

--- frontend/src/lib/EventCard.svelte
+++ frontend/src/lib/EventCard.svelte
@@ -1,6 +1,6 @@
 <script>
   import { Card, Button } from 'flowbite-svelte';
-  import { ArrowRightOutline } from 'flowbite-svelte-icons';
+  import { MapPinAltSolid } from 'flowbite-svelte-icons';
   import { PUBLIC_BACKEND_URL } from '$env/static/public';
 
   export let event;
@@ -11,18 +11,22 @@
 </script>
 
 <Card class="mt-4 sm:mx-2">
-  <img src={image} alt="Cover" class="mb-2 rounded-lg" />
+  <div class="flex items-center mb-2" style="margin-top: -0.75rem;"><MapPinAltSolid class="mr-2" /> <span class="text-xl">{event.location}</span></div>
+
+  <div class="mb-2 rounded-lg overflow-hidden" style="position: relative; padding-top: 56.25%;">
+    <img src={image} alt="Cover" class="absolute top-0 left-0 w-full h-full" style="object-fit: cover" />
+  </div>
+
+
   {#if event.start_date}
     <p>{new Date(event.start_date).toLocaleDateString('sv-SE')} - {new Date(event.end_date).toLocaleDateString('sv-SE')}</p>
   {:else}
     <p>{new Date(event.date).toLocaleDateString('sv-SE')} {new Date(event.date).toLocaleTimeString('sv-SE')}</p>
   {/if}
+
   <h1 class="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{event.title}</h1>
   <h2 class="mb-2">{event.description}</h2>
 
-  {#if event.body}
-    <p class="mb-2 font-normal text-gray-700 dark:text-gray-400 leading-tight">{event.body}</p>
-  {/if}
   <Button href={url ? url : event.url} class="w-full mt-auto shadow">
     {button}
   </Button>
